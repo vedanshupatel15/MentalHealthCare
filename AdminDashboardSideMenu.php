acbc9a5ec3767eef637a78655ccae460e080f027
vedanshupatel15 <987561@win.edu.au>
2025-05-20 18:24:12 +0530

    Admin dashboard side menu

---
+++ AdminDashboardSideMenu.php
@@ -0,0 +1,8 @@
+<ul>
+  <li><a href="AdminPatientDetails.php">Patient Details</a></li>
+  <li><a href="AdminMentalHeathDetails.php">Patient Mental Health Details</a></li>
+  <li><a href="AdminPatientHealthProgress.php">Patient Mental Health Progress</a></li>
+  <li><a href="AdminDoctorDetails.php">Doctor Details</a></li>
+  <li><a href="AdminPatientAppoinment.php">Patient Appointment Details</a></li>
+  <li><a href="AdminViewEnquiry.php">View Inquiry</a></li>
+</ul>
