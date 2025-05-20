be02148d6dd570a1581953d8347f61205e8972f5
vedanshupatel15 <987561@win.edu.au>
2025-05-20 18:24:52 +0530

    Doctor dashboard side menu

---
+++ DoctorDashboardSideMenu.php
@@ -0,0 +1,6 @@
+<ul>
+  <li><a href="WeekSchedule.php">Weekly Schedule</a></li>
+  <li><a href="DoctorViewAppoinment.php">View Appointment</a></li>
+  <li><a href="DoctorViewPatientDetails.php">Patient Details</a></li>
+  <li><a href="DoctorViewPatientProgress.php">Patient Progress</a></li>
+</ul>
